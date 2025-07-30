f42d7448fc206a4dc3b5321f74e5d89dae842b8c
Mowattabr <allan.villalobos.calderon@gmail.com>
2025-07-30 01:58:58 -0600

    Create user is ok

---
+++ Lab6/Lab6_test.php
@@ -0,0 +1,18 @@
+<?php
+$mensaje = "Hello, World!";
+$suma = 2 + 3;
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Lab6</title>
+</head>
+<body>
+    <?php
+    echo "<h1>$mensaje</h1>";
+    echo "<h2>$suma</h2>";
+    ?>
+</body>
+</html>
